0231ba14719cb1537430e01a9f08e5afb15b4141
Kumarforlearning <Kumarlearninghere@gmail.com>
2026-02-16 21:20:46 -0600

    Modify print statements in Main.java
    Updated print statements to reflect new changes.

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("New Change !");
+        System.out.println("New Change 1");
         System.out.println("New Change 2");
+        System.out.println("New Change 3");
     }
 }
